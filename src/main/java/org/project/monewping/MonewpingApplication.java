085458b96a8c0f04c6511aabf5da02218e8f530c
BackEunHo <bhh0214@gmail.com>
2025-07-11 19:33:48 +0900

    chore #31: 스프링 부트 프로젝트 개발 환경 설정
    - build.gradle 외부 의존성 추가,jacoco 설정 정의

---
+++ src/main/java/org/project/monewping/MonewpingApplication.java
@@ -0,0 +1,13 @@
+package org.project.monewping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonewpingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MonewpingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/project/monewping/MonewpingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.monewping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonewpingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
