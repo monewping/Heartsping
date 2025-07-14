2c04ba304bff1d0318833382646508b0668742b9
adjoon1 <minj97@naver.com>
2025-07-15 04:04:40 +0900

    feat #12: 댓글 응답 DTO 추가

--- src/main/java/org/project/monewping/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/org/project/monewping/domain/comment/dto/CommentResponseDto.java
@@ -4,13 +4,17 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * 댓글 응답 DTO
  * 댓글 조회 API 응답에 사용됩니다.
  */
+@Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CommentResponseDto {
     private UUID id;
     private String content;
