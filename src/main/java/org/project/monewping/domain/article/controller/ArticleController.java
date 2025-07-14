5e01f2c3cc7023e7ee007dd22e2d0d8abeb830a4
GeonMin02 <geonmin10@gmail.com>
2025-07-14 13:10:49 +0900

    feat #8 : 뉴스 기사 뷰 등록
    - ArticleController 작성

--- src/main/java/org/project/monewping/domain/article/controller/ArticleController.java
+++ src/main/java/org/project/monewping/domain/article/controller/ArticleController.java
@@ -1,7 +1,15 @@
 package org.project.monewping.domain.article.controller;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.project.monewping.domain.article.dto.data.ArticleViewDto;
+import org.project.monewping.domain.article.service.ArticleViewService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class ArticleController {
+
+    private final ArticleViewService articleViewService;
+
+    // 뉴스 기사 뷰 등록
+    @PostMapping("/{articleId}/article-views")
+    public ResponseEntity<ArticleViewDto> registerArticleView(
+        @PathVariable UUID articleId,
+        @RequestHeader("Monew-Request-User-ID") UUID viewedBy
+    ) {
+        log.info("기사 조회 등록 요청 수신 : userId = {}, articleId = {}", viewedBy, articleId);
+
+        LocalDateTime articlePublishedDate = LocalDateTime.now();
+
+        ArticleViewDto requestDto = new ArticleViewDto(
+            UUID.randomUUID(),
+            viewedBy,
+            articleId,
+            articlePublishedDate
+        );
+
+        ArticleViewDto responseDto = articleViewService.registerView(requestDto);
+        log.info("기사 조회 등록 완료 : id = {}, userId = {}, articleId = {}, publishedDate = {}",
+            requestDto.id(),
+            responseDto.viewedBy(),
+            responseDto.articleId(),
+            responseDto.articlePublishedDate());
+
+        return ResponseEntity.ok(responseDto);
+    }
 
 
 
