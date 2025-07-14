2911240e30b52e57edffe67d2e9d1b0b250074d5
Im-Ubin <yuvin3493@gmail.com>
2025-07-14 11:55:33 +0900

    feat #18: Notification Mapper 구현, 커스텀 예외 클래스 구현
    - NotificationMapper
    - UnsupportedResourceTypeException

---
+++ src/main/java/org/project/monewping/domain/notification/exception/UnsupportedResourceTypeException.java
@@ -0,0 +1,17 @@
+package org.project.monewping.domain.notification.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 지원하지 않는 리소스 타입인 경우 발생하는 예외입니다.
+ *
+ * <p>알림 등록 시, 지원하지 않는 리소스 타입인 경우
+ * 400 Bad Request 응답이 반환됩니다.</p>
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedResourceTypeException extends RuntimeException {
+    public UnsupportedResourceTypeException(String resourceType) {
+        super("지원하지 않는 리소스 타입입니다: " + resourceType);
+    }
+}
