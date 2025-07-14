1b3fd5df2c593a8d9f50a47ca8a300f930d5db8b
GeonMin02 <geonmin10@gmail.com>
2025-07-14 18:04:45 +0900

    feat #10 : 뉴스 기사 수집
    - Article Entity 작성

--- src/main/java/org/project/monewping/domain/article/entity/Articles.java
+++ src/main/java/org/project/monewping/domain/article/entity/Articles.java
@@ -1,5 +1,50 @@
 package org.project.monewping.domain.article.entity;
 
-public class Articles {
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.project.monewping.global.base.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table
+public class Articles extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interest_id", nullable = false)
+    private Interest interest;
+
+    @Column(nullable = false, length = 10)
+    private String source;
+
+    @Column(name = "original_link", nullable = false, length = 300, unique = true)
+    private String originalLink;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String summary;
+
+    @Column(name = "published_at",  nullable = false)
+    private LocalDateTime publishedAt;
+
+    @Column(name = "view_count", nullable = false)
+    private int viewCount;
+
+    @Column(nullable = false, name = "is_deleted")
+    private boolean deleted;
 
 }
