91b015103baab108995514784c95c4528a3e3153
adjoon1 <minj97@naver.com>
2025-07-15 04:03:55 +0900

    feat #12: 댓글 등록/조회용 Mapper 인터페이스 추가

--- src/main/java/org/project/monewping/domain/comment/mapper/CommentMapper.java
+++ src/main/java/org/project/monewping/domain/comment/mapper/CommentMapper.java
@@ -1,14 +1,14 @@
 package org.project.monewping.domain.comment.mapper;
 
-import org.mapstruct.Mapper;
 import org.project.monewping.domain.comment.domain.Comment;
+import org.project.monewping.domain.comment.dto.CommentRegisterRequestDto;
 import org.project.monewping.domain.comment.dto.CommentResponseDto;
 
 /**
  * 댓글 매퍼
  * Comment 엔티티와 CommentResponseDto 간 변환을 담당합니다.
  */
-@Mapper(componentModel = "spring")
 public interface CommentMapper {
     CommentResponseDto toResponseDto(Comment comment);
+    Comment toEntity(CommentRegisterRequestDto requestDto);
 }
