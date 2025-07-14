be0c120baf86d2ecb1222817dce5749bdf07b184
Im-Ubin <yuvin3493@gmail.com>
2025-07-14 13:36:42 +0900

    chore #18: 사용하지 않는 import문 제거
    - BasicNotificationService

--- src/main/java/org/project/monewping/domain/notification/service/BasicNotificationService.java
+++ src/main/java/org/project/monewping/domain/notification/service/BasicNotificationService.java
@@ -1,8 +1,5 @@
 package org.project.monewping.domain.notification.service;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
