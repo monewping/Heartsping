b9113cc5856b6c9f02126511357611d12600116d
adjoon1 <minj97@naver.com>
2025-07-17 10:24:49 +0900

    feat #14: 댓글 수정 요청 DTO 추가

---
+++ src/main/java/org/project/monewping/domain/comment/dto/CommentUpdateRequestDto.java
@@ -0,0 +1,19 @@
+package org.project.monewping.domain.comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * 댓글 수정 요청 DTO.
+ * <p>
+ * 댓글 수정 시 사용자가 입력한 내용을 전달하기 위한 객체입니다.
+ * <p>
+ * 필드:
+ * - content: 수정할 댓글 내용 (필수)
+ *
+ * @param content 수정할 댓글 내용
+ */
+public record CommentUpdateRequestDto(
+    @NotBlank(message = "댓글 내용은 필수입니다.")
+    String content
+) {
+}
