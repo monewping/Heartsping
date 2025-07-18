9ceb06e7584c0a663d7dc4ecce8c638172440ac8
adjoon1 <minj97@naver.com>
2025-07-19 06:55:20 +0900

    feat #16: CommentLike 엔티티 추가

---
+++ src/main/java/org/project/monewping/domain/comment/exception/CommentLikeNotFoundException.java
@@ -0,0 +1,7 @@
+package org.project.monewping.domain.comment.exception;
+
+public class CommentLikeNotFoundException extends RuntimeException {
+  public CommentLikeNotFoundException(String message) {
+    super(message);
+  }
+}
