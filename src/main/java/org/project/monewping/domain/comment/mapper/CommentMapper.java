de106a79f6359a257c3b86343f321baf9204cd9d
adjoon1 <minj97@naver.com>
2025-07-20 05:21:09 +0900

    refactor #16: 댓글 등록 시 userNickname을 파라미터로 받도록 Mapper 수정

--- src/main/java/org/project/monewping/domain/comment/mapper/CommentMapper.java
+++ src/main/java/org/project/monewping/domain/comment/mapper/CommentMapper.java
@@ -1,12 +1,13 @@
 package org.project.monewping.domain.comment.mapper;
 
 import org.project.monewping.domain.comment.domain.Comment;
+import org.project.monewping.domain.comment.dto.CommentRegisterRequestDto;
 import org.project.monewping.domain.comment.dto.CommentResponseDto;
 
 /**
- * 댓글 매퍼
- * Comment 엔티티와 CommentResponseDto 간 변환을 담당합니다.
+ * 댓글 Mapper 인터페이스
  */
 public interface CommentMapper {
+    Comment toEntity(CommentRegisterRequestDto requestDto, String userNickname);
     CommentResponseDto toResponseDto(Comment comment);
 }
