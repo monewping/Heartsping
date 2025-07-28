3fb0ca5a9aba87e5a78151bfeb960b38c49de740
HuInDoL <80470052+HuInDoL@users.noreply.github.com>
2025-07-28 10:49:48 +0900

    docs #96: 코드리뷰 반영 자바독 추가

--- src/main/java/org/project/monewping/domain/interest/exception/DuplicateSubscriptionException.java
+++ src/main/java/org/project/monewping/domain/interest/exception/DuplicateSubscriptionException.java
@@ -1,5 +1,11 @@
 package org.project.monewping.domain.interest.exception;
 
+/**
+ * 중복된 구독으로 발생하는 예외입니다.
+ *
+ * <p>이미 구독 중인 관심사를 다시 구독하려고 할 때 발생하며
+ * HTTP 409 상태와 함께 반환됩니다.</p>
+ */
 public class DuplicateSubscriptionException extends RuntimeException {
     public DuplicateSubscriptionException(String message) {
         super(message);
--- src/main/java/org/project/monewping/domain/interest/exception/InvalidRequestException.java
+++ src/main/java/org/project/monewping/domain/interest/exception/InvalidRequestException.java
@@ -1,5 +1,12 @@
 package org.project.monewping.domain.interest.exception;
 
+/**
+ * 잘못된 요청으로 발생하는 예외입니다.
+ *
+ * <p>키워드가 null이거나 빈 리스트인 경우 등
+ * 유효하지 않은 요청 데이터로 인해 발생하며
+ * HTTP 400 상태와 함께 반환됩니다.</p>
+ */
 public class InvalidRequestException extends RuntimeException {
     public InvalidRequestException(String message) {
         super(message);
